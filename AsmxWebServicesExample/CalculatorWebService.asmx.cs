281edb232d03b3687493488a71dac1f03064f41c
salihavci <cacabeystjr@gmail.com>
2022-11-24 21:23:06 +0300

    Proje dosyası ekle.

---
+++ AsmxWebServicesExample/CalculatorWebService.asmx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AsmxWebServicesExample
+{
+    /// <summary>
+    /// CalculatorWebService için özet açıklama
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Bu Web Hizmeti'nin, ASP.NET AJAX kullanılarak komut dosyasından çağrılmasına, aşağıdaki satırı açıklamadan kaldırmasına olanak vermek için.
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod(EnableSession = true,Description = "Bu metot 2 sayıyı toplayıp session'a kaydeder",CacheDuration = 15,MessageName = "DoubleSum")]
+        public int Add(int num1, int num2)
+        {
+            List<string> calculations;
+            if (Session["calculation"] == null)
+            {
+                calculations= new List<string>();
+            }
+            else
+            {
+                calculations = (List<string>)Session["calculation"];
+            }
+            var total = num1 + num2;
+            string strRecentCalculations = num1.ToString() + " + " + num2.ToString() + " = " + total.ToString();
+            calculations.Add(strRecentCalculations);
+            Session["calculation"] = calculations;
+            return total;
+        }
+        
+        [WebMethod(EnableSession = true,Description = "Bu metot 3 sayıyı toplayıp session'a kaydeder",CacheDuration = 15,MessageName = "TripleSum")]
+        public int Add(int num1, int num2,int num3)
+        {
+            List<string> calculations;
+            if (Session["calculation"] == null)
+            {
+                calculations= new List<string>();
+            }
+            else
+            {
+                calculations = (List<string>)Session["calculation"];
+            }
+            var total = num1 + num2 + num3;
+            string strRecentCalculations = num1.ToString() + " + " + num2.ToString() + " + " + num3.ToString() + " = " + total.ToString();
+            calculations.Add(strRecentCalculations);
+            Session["calculation"] = calculations;
+            return total;
+        }
+
+        [WebMethod(EnableSession = true, Description = "Bu metot kaydedilen işlemleri liste olarak getirir.")]
+        public List<string> GetCalculations()
+        {
+            if (Session["calculation"] == null)
+            {
+                List<string> calculations = new List<string>
+                {
+                    "Herhangi bir hesaplama yapılmadı."
+                };
+                 return calculations;
+            }
+            else
+            {
+                return (List<string>)Session["calculation"];
+            }
+        }
+    }
+}
---
+++ AsmxWebServicesExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("AsmxWebServicesExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AsmxWebServicesExample")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğini false olarak ayarlarsanız COM bileşenleri bu 
+// derlemedeki türleri göremez. Bu derlemedeki bir türe COM'dan erişmeniz 
+// gerekiyorsa, o türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("84a9a0ce-40e1-4d98-8132-d4cee69c9159")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilirsiniz veya Düzeltme ve Yapı Numaralarına aşağıda 
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
