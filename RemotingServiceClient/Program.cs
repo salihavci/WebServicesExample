5756f3569c3cd40bdcba4bb17d0a1b75683fd178
salihavci <cacabeystjr@gmail.com>
2022-11-29 19:52:58 +0300

    WebServices Remoting Service Example

---
+++ RemotingServiceClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using RemotingServicesExample;
+
+namespace RemotingServiceClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RemotingService service;
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel, false);
+            service = (RemotingService)Activator.GetObject(typeof(RemotingService), "tcp://localhost:2065/GetMessage");
+            Console.WriteLine(service.GetMessage("Salih"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RemotingServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using RemotingServicesExample;
+
+namespace RemotingServiceHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RemotingService service = new RemotingService();
+            TcpChannel channel = new TcpChannel(2065);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(RemotingService), "GetMessage", WellKnownObjectMode.Singleton);
+            Console.WriteLine($"Remoting servis başlatıldı : {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
